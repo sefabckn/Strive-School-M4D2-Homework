7db3dd44825bb378e7ea196c0b3ab4f6676d67f7
sefabckn <sefabckn@gmail.com>
2021-11-09 15:17:25 +0100

    files created, Navbar component created and added to App.js and rendered. Displayed correctly

---
+++ strivebooks/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import MyNavbar from './components/MyNavbar'
+
+
+function App() {
+  return (
+    <div>
+      <MyNavbar brand="Strivebook" />
+
+    </div> 
+  );
+}
+
+export default App;
