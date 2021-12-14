a0bbdc2a7520db8c623e067c15d41a941b5c1fff
SefaBckn <31974256+sefabckn@users.noreply.github.com>
2021-12-14 16:32:48 +0100

    state updated

--- strivebooks/src/components/LatestRelease.jsx
+++ strivebooks/src/components/LatestRelease.jsx
@@ -1,14 +1,15 @@
 //Create a Latest Release component. 
 //This component should read from one of the provided 
 //.json files and print out the cover of the books.
-//import { useEffect } from 'react'
+
 import { useState } from 'react'
 import {Container, Row, Col, Card, Button} from 'react-bootstrap'
-import strive from '../scifi.json'
+import items from '../scifi.json'
 import { Component } from 'react'
-/* const LatestRelease = () =>{
-    const [chosenBook, setChosenBook] = useState(null) //hook usage
-   
+
+const LatestRelease = () =>{
+    const [chosenBook, setChosenBook] = useState(null) 
+
     return(
         <Container fluid>
         <Row class = "row mt-3">
@@ -19,7 +20,7 @@ import { Component } from 'react'
                                 <Card.Img 
                                 variant="top" 
                                 src={bookObject.img}
-                                onClick={()=>{ chosenBook === null && setChosenBook( bookObject )}}
+                                onClick={()=>{ setChosenBook( bookObject )}}
                                 />
                                 <Card.Body>
                                     <Card.Title>{bookObject.title}</Card.Title>
@@ -42,21 +43,24 @@ import { Component } from 'react'
 }
 
 export default LatestRelease
- */
 
-class LatestRelease extends Component {
 
+/* class LatestRelease extends Component {
+  
     state= {
         chosenBook : null
     }
-
     render(){
+        const myStyle = {
+            width: '12rem',
+            height: '100%',
+        }
         return(
             <Container fluid>
                 <Row class = "row mt-3">{
-                    strive.map((book) => (
+                    items.map((book) => (
                         <Col xs={12} s={6} md={4} xl ={2}>
-                        <Card style={{ width: '12rem' }}>
+                        <Card style={ myStyle }>
                             <Card.Img 
                             variant="top" 
                             src={book.img}
@@ -87,4 +91,4 @@ class LatestRelease extends Component {
     
 
 }
-export default LatestRelease
+export default LatestRelease */
--- strivebooks/src/components/MyFooter.jsx
+++ strivebooks/src/components/MyFooter.jsx
@@ -3,7 +3,7 @@
         //CSS styling added to index.css with .footer classname
     <footer class="footer">
       <div class="container">
-        <span class="text-muted">&#169; 2021 Strive Book</span>
+        <span class="text-muted text-align-center">&#169; 2021 Strive Book</span>
       </div>
     </footer>
       );
